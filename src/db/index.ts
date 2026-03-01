e36cbe72a1e5dd6e48fe5965794e46a33ec96cf9
JoseDv1 <jose123millon@gmail.com>
2026-03-01 12:35:18 -0500

    chore: Update database URL environment variable from `TURSO_DATABASE_URL` to `DATABASE_URL`.

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "./generated/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const adapter = new PrismaLibSql({
-    url: import.meta.env.TURSO_DATABASE_URL,
+    url: import.meta.env.DATABASE_URL,
     authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
 
